32389815cdffe8593115a95687c3d2f9dcff5244
Jaydeepsinh Parmar <148114163+jaydeep099@users.noreply.github.com>
2025-06-22 11:38:29 +0530

    College Final Year project

---
+++ app/page.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  ArrowRight,
+  Trophy,
+  Target,
+  Sparkles,
+  CheckCircle2,
+} from "lucide-react";
+import HeroSection from "@/components/hero";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Image from "next/image";
+import { features } from "@/data/features";
+import { testimonial } from "@/data/testimonial";
+import { faqs } from "@/data/faqs";
+import { howItWorks } from "@/data/howItWorks";
+
+export default function LandingPage() {
+  return (
+    <>
+      <div className="grid-background"></div>
+
+      {/* Hero Section */}
+      <HeroSection />
+    </>
+  );
+}
